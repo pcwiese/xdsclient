b9774b16d735c67da5454fea4cb22b00b0a4232a
jijie <jijie@someplace.com>
2020-12-23 23:20:11 +0800

    添加了 KubernetesClient 并获取 istiod-service-account 的密钥

--- XdsClient/IstioCAClient.cs
+++ XdsClient/IstioCAClient.cs
@@ -3,7 +3,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
+using k8s;
 
 namespace XdsClient
 {
@@ -11,7 +13,28 @@ namespace XdsClient
     {
         public static async Task<X509Certificate> CreateClientCertificateAsync(string uriSAN)
         {
+            var jwtToken = await RetriveIstiodSATokenAsync();
+
+            // https://github.com/istio/istio/security/pkg/nodeagent/caclient/providers/citadel/client.go
+            // https://github.com/istio/api/blob/master/security/v1alpha1/ca.pb.go#L212
+            // "/istio.v1.auth.IstioCertificateService/CreateCertificate
+            
+            // kubectl get serviceaccount/istiod-service-account -n istio-system -o 'jsonpath={.secrets[0].name}'
+            // kubectl get secret/istiod-service-account-token-v55nh -n istio-system -o 'jsonpath={.data.token}'
+            
             return new X509Certificate2(@"C:\Users\jijie\Documents\Projects\XdsClient\XdsClient\Certs\client.pfx", string.Empty);
+        }
+
+        private static async Task<string> RetriveIstiodSATokenAsync()
+        {
+            const string istioNamespace = "istio-system";
+
+            var kubeClient = new Kubernetes(KubernetesClientConfiguration.BuildDefaultConfig());
+            var istiodServiceAccount = await kubeClient.ReadNamespacedServiceAccountAsync("istiod-service-account", istioNamespace);
+            var secretName = istiodServiceAccount.Secrets[0].Name;
+
+            var saSecret = await kubeClient.ReadNamespacedSecretAsync(secretName, istioNamespace);
+            return Encoding.ASCII.GetString(saSecret.Data["token"]);
         }
 
         public static async Task<X509Certificate> GetIstiodCACertAsync()
