eabe0b1ced84f8a62dcf10f4fd99d72b67a5866f
jijie <jijie@someplace.com>
2020-12-27 15:52:23 +0800

    现在具备获取 endpoints 及 routes 的能力

--- XdsClient/Program.cs
+++ XdsClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Envoy.Config.Cluster.V3;
@@ -10,6 +11,7 @@ using Envoy.Config.Core.V3;
 using Envoy.Config.Endpoint.V3;
 using Envoy.Config.Listener.V3;
 using Envoy.Config.Route.V3;
+using Envoy.Extensions.Filters.Network.HttpConnectionManager.V3;
 using Envoy.Service.Discovery.V3;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
@@ -63,18 +65,25 @@ namespace XdsClient
             var resources = new XdsResources();
             var streamingCall = adsClient.StreamAggregatedResources();
 
-            resources.Clusters = await FetchResources<Cluster>(nodeId, EnvoyTypeConstants.ClusterType, streamingCall);
-            resources.Endpoints = await FetchResources<Endpoint>(nodeId, EnvoyTypeConstants.EndpointType, streamingCall);
+            resources.Clusters = await FetchResources<Cluster>(nodeId, EnvoyTypeConstants.ClusterType, null, streamingCall);
+
+            var endpointNames = resources.Clusters
+                .Select(c => c.EdsClusterConfig?.ServiceName)
+                .Where(e => e != null)
+                .Distinct()
+                .ToList();
+            resources.Endpoints = await FetchResources<ClusterLoadAssignment>(nodeId, EnvoyTypeConstants.EndpointType, endpointNames, streamingCall);
             
-            resources.Listeners = await FetchResources<Listener>(nodeId, EnvoyTypeConstants.ListenerType, streamingCall);
-            resources.Routes = await FetchResources<Route>(nodeId, EnvoyTypeConstants.RouteType, streamingCall);
+            resources.Listeners = await FetchResources<Listener>(nodeId, EnvoyTypeConstants.ListenerType, null, streamingCall);
+            var routeNames = GetRouteNames(resources.Listeners);
+            resources.Routes = await FetchResources<RouteConfiguration>(nodeId, EnvoyTypeConstants.RouteType, routeNames, streamingCall);
             
-            resources.Extensions = await FetchResources<Extension>(nodeId, EnvoyTypeConstants.ExtensionConfigType, streamingCall);
+            resources.Extensions = await FetchResources<Extension>(nodeId, EnvoyTypeConstants.ExtensionConfigType, null, streamingCall);
             
             return resources;
         }
 
-        private static async Task<List<T>> FetchResources<T>(string nodeId, string resourceTypeString, AsyncDuplexStreamingCall<DiscoveryRequest, DiscoveryResponse> call) where T : IMessage, new()
+        private static async Task<List<T>> FetchResources<T>(string nodeId, string resourceTypeString, List<string> resourceNames, AsyncDuplexStreamingCall<DiscoveryRequest, DiscoveryResponse> call) where T : IMessage, new()
         {
             var request = new DiscoveryRequest
             {
@@ -85,6 +94,10 @@ namespace XdsClient
                 }, 
                 TypeUrl = resourceTypeString
             };
+            if (resourceNames != null)
+            {
+                request.ResourceNames.AddRange(resourceNames);
+            }
             await call.RequestStream.WriteAsync(request);
             
             await call.ResponseStream.MoveNext(CancellationToken.None);
@@ -105,14 +118,33 @@ namespace XdsClient
             return (grpcConnection, new AggregatedDiscoveryService.AggregatedDiscoveryServiceClient(grpcConnection));
         }
 
+        private static List<string> GetRouteNames(List<Listener> listeners)
+        {
+            return listeners.Select(l =>
+                {
+                    var httpFilterChain = l.FilterChains?.FirstOrDefault(chain =>
+                        chain.FilterChainMatch?.ApplicationProtocols != null &&
+                        chain.FilterChainMatch.ApplicationProtocols.Contains("http/1.1"));
+                    var hcm = httpFilterChain?.Filters?.FirstOrDefault(f => f.Name == "envoy.http_connection_manager");
+                    return hcm?.TypedConfig;
+                }).Where(hcmConfig => hcmConfig != null)
+                .Select(hcmConfig =>
+                {
+                    var hcm = hcmConfig.Unpack<HttpConnectionManager>();
+                    return hcm.Rds?.RouteConfigName;
+                })
+                .Where(r => r != null)
+                .Distinct()
+                .ToList();
+        }
 
         class XdsResources
         {
             public List<Cluster> Clusters { get; set; }
-            public List<Endpoint> Endpoints { get; set; }
+            public List<ClusterLoadAssignment> Endpoints { get; set; }
             
             public List<Listener> Listeners { get; set; }
-            public List<Route> Routes { get; set; }
+            public List<RouteConfiguration> Routes { get; set; }
             
             public List<Extension> Extensions { get; set; }
         }
